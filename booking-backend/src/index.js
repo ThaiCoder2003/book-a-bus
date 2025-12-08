ccbe8837dfb313d21bd04f6b6f7b495474fb0d8e
Dongz125 <hoangkhai3122004@gmail.com>
2025-12-08 11:18:50 +0700

    merge admin n user

--- booking-backend/src/index.js
+++ booking-backend/src/index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 require('dotenv').config();
 
 const routes = require('./routes/index');
-const adminRoutes = require('./routes/adminRoutes/index');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes);
-app.use('/api/admin', adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
