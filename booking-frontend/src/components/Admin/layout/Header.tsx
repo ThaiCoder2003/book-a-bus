1f23f061706c8b494003b1af34288315f98266b6
Le Phan Thanh Nhan <136586134+lephanthanhnhan@users.noreply.github.com>
2025-12-17 15:09:21 +0700

    add layout and Ui components

---
+++ booking-frontend/src/components/Admin/layout/Header.tsx
@@ -0,0 +1,45 @@
+export default function Header() {
+  return (
+    <header className="bg-white p-4 border-b flex justify-between items-center">
+      <div className="relative w-1/3 max-w-sm">
+        
+      </div>
+      <div className="flex items-center space-x-3">
+        <div className="relative p-2 rounded-full bg-gray-100 hover:bg-gray-200 cursor-pointer transition-colors">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-gray-600"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={1.5}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+          </svg>
+
+          <span className="absolute top-1 right-1 h-2 w-2 bg-red-500 rounded-full border-2 border-white"></span>
+        </div>
+        <div className="p-2 rounded-full bg-gray-100 hover:bg-gray-200 cursor-pointer transition-colors">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-gray-600"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={1.5}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+            />
+          </svg>
+        </div>
+      </div>
+    </header>
+  );
+}
