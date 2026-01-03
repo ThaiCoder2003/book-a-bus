30df426fa331c41ef81036f9d9450c32267ca113
Le Phan Thanh Nhan <136586134+lephanthanhnhan@users.noreply.github.com>
2026-01-03 18:28:51 +0700

    Update transaction.mock.ts

--- booking-frontend/src/data/transaction.mock.ts
+++ booking-frontend/src/data/transaction.mock.ts
@@ -3,58 +3,29 @@ import type { TransactionUI } from "@/types/ui/transaction.ui";
 export const TRANSACTION_MOCK: TransactionUI[] = [
   {
     id: "1",
-    userName: "User A",
-    userEmail: "user.a@example.com",
-    amount: 200000,
+    totalAmount: 200000,
     status: "CONFIRMED",
+    user: {
+      name: "User A",
+      email: "user.a@example.com",
+    },
   },
   {
     id: "2",
-    userName: "User B",
-    userEmail: "user.b@example.com",
-    amount: 150000,
+    totalAmount: 150000,
     status: "PENDING",
+    user: {
+      name: "User B",
+      email: "user.b@example.com",
+    },
   },
   {
     id: "3",
-    userName: "User C",
-    userEmail: "user.c@example.com",
-    amount: 300000,
+    totalAmount: 300000,
     status: "CANCELLED",
-  },
-  {
-    id: "3",
-    userName: "User C",
-    userEmail: "user.c@example.com",
-    amount: 300000,
-    status: "CANCELLED",
-  },
-  {
-    id: "3",
-    userName: "User C",
-    userEmail: "user.c@example.com",
-    amount: 300000,
-    status: "CANCELLED",
-  },
-  {
-    id: "3",
-    userName: "User C",
-    userEmail: "user.c@example.com",
-    amount: 300000,
-    status: "CANCELLED",
-  },
-  {
-    id: "3",
-    userName: "User C",
-    userEmail: "user.c@example.com",
-    amount: 300000,
-    status: "CANCELLED",
-  },
-  {
-    id: "3",
-    userName: "User C",
-    userEmail: "user.c@example.com",
-    amount: 300000,
-    status: "CANCELLED",
+    user: {
+      name: "User C",
+      email: "user.c@example.com",
+    },
   },
 ];
