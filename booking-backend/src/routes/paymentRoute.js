c575a48cf98ccc44b588a2be69912de7854d4293
Dongz125 <hoangkhai3122004@gmail.com>
2026-01-03 21:20:24 +0700

    fix payment

--- booking-backend/src/routes/paymentRoute.js
+++ booking-backend/src/routes/paymentRoute.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 const paymentController = require('../controllers/paymentController');
 
+const jsonParser = express.json();
+
 // ZaloPay gọi tự động (Webhook)
-router.post('/zalopay/callback', paymentController.callbackZaloPay);
+router.post('/zalopay/callback', jsonParser, paymentController.callbackZaloPay);
 router.post('/zalopay/:bookingId', authMiddleware.verifyToken, paymentController.createPayPayment);
 
 
