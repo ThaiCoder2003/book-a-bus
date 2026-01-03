ff367fb2ffd517c5c753817ffba49e127c81dd90
Dongz125 <hoangkhai3122004@gmail.com>
2026-01-03 21:40:08 +0700

    fix be

--- booking-backend/src/routes/stationRoute.js
+++ booking-backend/src/routes/stationRoute.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 const stationController = require('../controllers/stationController');
 
-router.get('/', authMiddleware.verifyToken, stationController.getAllStations);
-router.post('/', authMiddleware.verifyToken, authMiddleware.requireAdmin, stationController.registerNewStation);
 router.put('/:id', authMiddleware.verifyToken, authMiddleware.requireAdmin, stationController.updateStation);
 router.delete('/:id', authMiddleware.verifyToken, authMiddleware.requireAdmin, stationController.deleteStation);
+router.get('/', authMiddleware.verifyToken, stationController.getAllStations);
+router.post('/', authMiddleware.verifyToken, authMiddleware.requireAdmin, stationController.registerNewStation);
 
 module.exports = router;
--- booking-backend/src/routes/tripRoute.js
+++ booking-backend/src/routes/tripRoute.js
@@ -3,11 +3,11 @@ const router = express.Router()
 const authMiddleware = require('../middlewares/authMiddleware')
 const tripController = require('../controllers/tripController')
 
-router.get('/', tripController.getAll)
 router.get('/getAll', tripController.getAllTrips)
 router.get('/:id', tripController.getTripDetail)
 router.post('/register', authMiddleware.verifyToken, authMiddleware.requireAdmin, tripController.registerNewTrip);
 router.post('/update/:id', authMiddleware.verifyToken, authMiddleware.requireAdmin, tripController.updateTrip);
 router.delete('/delete/:id', authMiddleware.verifyToken, authMiddleware.requireAdmin, tripController.deleteTrip);
+router.get('/', tripController.getAll)
 
 module.exports = router
