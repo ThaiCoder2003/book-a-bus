800786f2156eb085e2190f21590895db6a8e4872
ThaiCoder2003 <101780284+ThaiCoder2003@users.noreply.github.com>
2026-01-05 15:19:19 +0700

    Update stationRoute.js

--- booking-backend/src/routes/stationRoute.js
+++ booking-backend/src/routes/stationRoute.js
@@ -6,6 +6,6 @@ const stationController = require('../controllers/stationController');
 router.put('/:id', authMiddleware.verifyToken, authMiddleware.requireAdmin, stationController.updateStation);
 router.delete('/:id', authMiddleware.verifyToken, authMiddleware.requireAdmin, stationController.deleteStation);
 router.get('/', authMiddleware.verifyToken, stationController.getAllStations);
-router.post('/', authMiddleware.verifyToken, authMiddleware.requireAdmin, stationController.registerNewStation);
+router.post('/register', authMiddleware.verifyToken, authMiddleware.requireAdmin, stationController.registerNewStation);
 
 module.exports = router;
